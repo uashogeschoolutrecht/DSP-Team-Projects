415b1dabb87f1291e27aac8e415b6636a65831cc
rinsrutgers <rins.rutgers@gmail.com>
2024-12-12 17:22:49 +0100

    fix profile

--- src/components/Profilecard/ProfileCard.js
+++ src/components/Profilecard/ProfileCard.js
@@ -2,9 +2,15 @@ import React from 'react';
 import styles from './styles.module.css';
 
 const ProfileCard = ({ name, title, avatarUrl }) => {
+  const baseUrl = process.env.PUBLIC_URL || ''; // Use PUBLIC_URL for proper asset resolution
+
   return (
     <div className={styles['member-card']}>
-      <img src={avatarUrl} alt={`${name}'s profile`} className={styles.avatar} />
+      <img 
+        src={`${baseUrl}${avatarUrl}`} // Prepend PUBLIC_URL to avatarUrl
+        alt={`${name}'s profile`} 
+        className={styles.avatar} 
+      />
       <div className={styles['member-name']}>{name}</div>
       <div className={styles.username}>{title}</div>
     </div>
